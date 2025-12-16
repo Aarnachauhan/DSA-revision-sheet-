fd4b763e237988ad0a8a0d1c5f9797d92c72f7b0
Aarna chauhan <chauhanaarna6@gmail.com>
2025-12-16 14:59:18 +0400

    Update top k freq elements.cpp

--- Arrays/top k freq elements.cpp
+++ Arrays/top k freq elements.cpp
@@ -1,1 +1,37 @@
 
+brute force :
+tc-o(n*k)
+sc-o(1)
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n=nums.size();
+      
+       unordered_map<int, int> freq;
+       for(auto it:nums){
+        freq[it]++;
+       }
+
+       vector<int> ans;
+       for(int i=0;i<k;i++){
+        int maxi=0 , element=0;
+        for(auto it:freq){
+            if(it.second> maxi){
+                maxi=it.second;
+                element=it.first;
+
+            }
+        }
+        ans.push_back(element);
+        freq.erase(element); //remove it from frequency map .
+       }
+       return ans;
+    }
+};
+
+better :
+tc-o()
+sc-o()
+
+  
