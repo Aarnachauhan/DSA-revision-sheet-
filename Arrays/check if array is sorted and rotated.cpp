e11fd3d82d5a73212ec185154c5d5be0ba642638
Aarna chauhan <chauhanaarna6@gmail.com>
2025-12-26 11:40:24 +0400

    Update check if array is sorted and rotated.cpp

--- Arrays/check if array is sorted and rotated.cpp
+++ Arrays/check if array is sorted and rotated.cpp
@@ -20,5 +20,18 @@ public:
     }
 };
 
+in python
+class Solution(object):
+    def check(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
 
-
+        cnt,n=0, len(nums)
+        for i in range(n):
+            if nums[i]>nums[(i+1)%n]:
+                cnt+=1
+                if cnt>1 : return False;
+        return True
+        
